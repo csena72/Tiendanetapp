6aa08bf54ca44a0c063e969aabb47397dd43eb5b
Cristian Sena <cristian.sena@extendeal.com>
2021-04-02 14:55:50 -0300

    Cart View - Sena

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,11 +1,34 @@
+import React, { useContext } from "react";
+import CartContext from "../contexts/CartContext";
 import { Link } from "react-router-dom";
 import { MdShoppingCart } from "react-icons/md";
-import { Button } from 'react-bootstrap';
+import { Button, Badge } from 'react-bootstrap';
 
 export function CartWidget() {
+
+  const context = useContext(CartContext);
+
+  const handleQty = () =>{
+    if(!context.cartState.length) {
+      return 0;
+    }
+    const qtys = context.cartState.map( product => {
+      return product.quantity;
+    });
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return qtys.reduce(reducer);
+  }
+
   return ( 
+    <>
+    { handleQty() > 0 && (
     <Link to={"/cart"}>
-      <Button variant="outline-info"><MdShoppingCart /> Comprar</Button>
-    </Link>   
+      <Button variant="outline-info"><MdShoppingCart />
+       Comprar <Badge variant="light">{ handleQty() }</Badge>
+       </Button>
+    </Link>
+    )}
+    </>   
   );
 }
